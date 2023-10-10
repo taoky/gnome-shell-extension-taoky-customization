85905cf21d1903ab3d2deff4c386c7b70aecf9b3
taoky <me@taoky.moe>
2023-10-10 17:04:37 +0800

    GNOME 45 porting (#2)
    * init work
    
    * Add lock screen session and remove unredirect tricks
    
    * Add eslint
    
    * Disable code of minimize animation

--- extension.js
+++ extension.js
@@ -16,12 +16,10 @@
  * SPDX-License-Identifier: GPL-2.0-or-later
  */
 
-/* exported init */
-
-const { Meta, Gio, GLib } = imports.gi;
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-const WindowManager = imports.ui.windowManager;
+import Meta from 'gi://Meta';
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
 function runCommand(cmd, comment) {
     let res = GLib.shell_parse_argv(cmd);
@@ -31,13 +29,12 @@ function runCommand(cmd, comment) {
         logError(`Glib.shell_parse_argv cannot parse cmd: ${cmd}`);
     } else {
         let proc = Gio.Subprocess.new(argv, Gio.SubprocessFlags.NONE);
-        proc.wait_check_async(null, (proc, result) => {
+        proc.wait_check_async(null, (proc_, result) => {
             try {
-                if (proc.wait_check_finish(result)) {
-                    log (`${comment} successfully executed`);
-                } else {
-                    logError (`the process to execute the ${comment} failed`);
-                }
+                if (proc_.wait_check_finish(result))
+                    log(`${comment} successfully executed`);
+                else
+                    logError(`the process to execute the ${comment} failed`);
             } catch (e) {
                 logError(e);
             }
@@ -45,80 +42,74 @@ function runCommand(cmd, comment) {
     }
 }
 
-class Extension {
-    constructor() {
+export default class MyExtension extends Extension {
+    constructor(metadata) {
+        super(metadata);
         this.disable_unredirect_count = 0;
         this.executed_xwayland_pointer_gestures_disable = false;
-        this.MINIMIZE_WINDOW_ANIMATION_TIME = null;
-        this.meta_enable_unredirect_for_display = null;
-        this.meta_disable_unredirect_for_display = null;
+        // this.MINIMIZE_WINDOW_ANIMATION_TIME = null;
+        // this.meta_enable_unredirect_for_display = null;
+        // this.meta_disable_unredirect_for_display = null;
     }
 
     enable() {
-        this.settings = ExtensionUtils.getSettings('org.gnome.shell.extensions.taoky-customization');
-        if (this.settings.get_value("disable-unredirect").get_boolean()) {
-            log ("Disabling unredirect for global display");
+        this.settings = this.getSettings('org.gnome.shell.extensions.taoky-customization');
+        if (this.settings.get_value('disable-unredirect').get_boolean()) {
+            log('Disabling unredirect for global display');
             Meta.disable_unredirect_for_display(global.display);
             this.disable_unredirect_count += 1;
         }
-        if (this.settings.get_value("disable-xwayland-pointer-gestures").get_boolean()) {
-            log ("Disabling xwayland pointer gestures");
-            runCommand("sh -c 'xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput disable'", "xwayland pointer gestures disable");
+        if (this.settings.get_value('disable-xwayland-pointer-gestures').get_boolean()) {
+            log('Disabling xwayland pointer gestures');
+            runCommand("sh -c 'xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput disable'", 'xwayland pointer gestures disable');
             this.executed_xwayland_pointer_gestures_disable = true;
         }
-        if (this.settings.get_value("disable-minimize-animation").get_boolean()) {
-            log ("Disabling minimize animation");
-            this.MINIMIZE_WINDOW_ANIMATION_TIME = WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME;
-            WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME = 0;
-        }
-        if (this.settings.get_value("monkey-patch-unredirect").get_boolean()) {
-            log ("Monkey patching Meta.enable_unredirect_for_display and Meta.disable_unredirect_for_display");
-            this.meta_enable_unredirect_for_display = Meta.enable_unredirect_for_display;
-            Meta.enable_unredirect_for_display = function() {
-                // Avoid spamming log
-                // log ("Calling Meta.enable_unredirect_for_display() as no-op.");
-            }
-            this.meta_disable_unredirect_for_display = Meta.disable_unredirect_for_display;
-            Meta.disable_unredirect_for_display = function() {
-                // Avoid spamming log
-                // log ("Calling Meta.disable_unredirect_for_display() as no-op.");
-            }
-        }
+        // if (this.settings.get_value('disable-minimize-animation').get_boolean()) {
+        //     log('Disabling minimize animation');
+        //     this.MINIMIZE_WINDOW_ANIMATION_TIME = WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME;
+        //     WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME = 0;
+        // }
+        // if (this.settings.get_value("monkey-patch-unredirect").get_boolean()) {
+        //     log ("Monkey patching Meta.enable_unredirect_for_display and Meta.disable_unredirect_for_display");
+        //     this.meta_enable_unredirect_for_display = Meta.enable_unredirect_for_display;
+        //     Meta.enable_unredirect_for_display = function() {
+        //         // Avoid spamming log
+        //         // log ("Calling Meta.enable_unredirect_for_display() as no-op.");
+        //     }
+        //     this.meta_disable_unredirect_for_display = Meta.disable_unredirect_for_display;
+        //     Meta.disable_unredirect_for_display = function() {
+        //         // Avoid spamming log
+        //         // log ("Calling Meta.disable_unredirect_for_display() as no-op.");
+        //     }
+        // }
     }
 
     disable() {
-        if (this.meta_enable_unredirect_for_display !== null) {
-            log ("Restoring Meta.enable_unredirect_for_display");
-            Meta.enable_unredirect_for_display = this.meta_enable_unredirect_for_display;
-        }
-        if (this.meta_disable_unredirect_for_display !== null) {
-            log ("Restoring Meta.disable_unredirect_for_display");
-            Meta.disable_unredirect_for_display = this.meta_disable_unredirect_for_display;
-        }
-        if (this.settings.get_value("dont-enable-unredirect-on-extension-stop").get_boolean()) {
-            this.disable_unredirect_count -= 1;
-        }
+        // if (this.meta_enable_unredirect_for_display !== null) {
+        //     log ("Restoring Meta.enable_unredirect_for_display");
+        //     Meta.enable_unredirect_for_display = this.meta_enable_unredirect_for_display;
+        // }
+        // if (this.meta_disable_unredirect_for_display !== null) {
+        //     log ("Restoring Meta.disable_unredirect_for_display");
+        //     Meta.disable_unredirect_for_display = this.meta_disable_unredirect_for_display;
+        // }
+        // if (this.settings.get_value("dont-enable-unredirect-on-extension-stop").get_boolean()) {
+        //     this.disable_unredirect_count -= 1;
+        // }
         for (let i = 0; i < this.disable_unredirect_count; i++) {
-            log ("Enabling unredirect for global display");
+            log('Enabling unredirect for global display');
             Meta.enable_unredirect_for_display(global.display);
         }
         this.disable_unredirect_count = 0;
         if (this.executed_xwayland_pointer_gestures_disable) {
-            log ("Enabling xwayland pointer gestures");
-            runCommand("sh -c 'xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput enable'", "xwayland pointer gestures enable");
+            log('Enabling xwayland pointer gestures');
+            runCommand("sh -c 'xinput list --name-only | grep ^xwayland-pointer-gestures | xargs -n1 xinput enable'", 'xwayland pointer gestures enable');
             this.executed_xwayland_pointer_gestures_disable = false;
         }
-        if (this.MINIMIZE_ANIMATION_TIME !== null) {
-            log (`Restoring minimize animation (time to ${this.MINIMIZE_WINDOW_ANIMATION_TIME})`);
-            WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME = this.MINIMIZE_WINDOW_ANIMATION_TIME;
-            this.MINIMIZE_WINDOW_ANIMATION_TIME = null;
-        }
+        // if (this.MINIMIZE_ANIMATION_TIME !== null) {
+        //     log(`Restoring minimize animation (time to ${this.MINIMIZE_WINDOW_ANIMATION_TIME})`);
+        //     WindowManager.MINIMIZE_WINDOW_ANIMATION_TIME = this.MINIMIZE_WINDOW_ANIMATION_TIME;
+        //     this.MINIMIZE_WINDOW_ANIMATION_TIME = null;
+        // }
     }
 }
-
-function init() {
-    log(`initializing ${Me.metadata.name}`);
-    ExtensionUtils.initTranslations(Me.metadata.uuid);
-    return new Extension();
-}
-
--- prefs.js
+++ prefs.js
@@ -1,20 +1,11 @@
-'use strict';
-
-const { Adw, Gio, Gtk } = imports.gi;
-
-const ExtensionUtils = imports.misc.extensionUtils;
-const Me = ExtensionUtils.getCurrentExtension();
-
-const Gettext = imports.gettext;
-const Domain = Gettext.domain(Me.metadata.uuid);
-const _ = Domain.gettext;
+import Adw from 'gi://Adw';
+import Gio from 'gi://Gio';
+import Gtk from 'gi://Gtk';
+import {ExtensionPreferences, gettext as _} from 'resource:///org/gnome/Shell/Extensions/js/extensions/prefs.js';
 
 
-function init() {
-}
-
-function addBooleanRow(settings, title, name, group, subtitle = null) {
-    const row = new Adw.ActionRow({ title });
+function addBooleanRow({settings, title, name, group, subtitle = null, disabled = false}) {
+    const row = new Adw.ActionRow({title});
     group.add(row);
 
     const toggle = new Gtk.Switch({
@@ -30,40 +21,51 @@ function addBooleanRow(settings, title, name, group, subtitle = null) {
 
     row.add_suffix(toggle);
     row.activatable_widget = toggle;
-    if (subtitle) {
+    if (subtitle)
         row.subtitle = subtitle;
+    row.set_sensitive(!disabled);
+}
+
+export default class MyExtensionPreferences extends ExtensionPreferences {
+    fillPreferencesWindow(window) {
+        // Use the same GSettings schema as in extension.js
+        const settings = this.getSettings(
+            'org.gnome.shell.extensions.taoky-customization');
+
+        // Create a preferences page and group
+        const page = new Adw.PreferencesPage();
+        const group_startup = new Adw.PreferencesGroup();
+        group_startup.set_title(_('On extension startup'));
+        page.add(group_startup);
+
+        addBooleanRow({
+            settings, title: _('Disable unredirection'),
+            name: 'disable-unredirect', group: group_startup,
+        });
+        addBooleanRow({
+            settings, title: _('Disable xwayland pointer gestures'),
+            name: 'disable-xwayland-pointer-gestures', group: group_startup,
+        });
+        addBooleanRow({
+            settings, title: _('Disable minimize animation'),
+            name: 'disable-minimize-animation', group: group_startup,
+            subtitle: 'This no longer works in GNOME 45', disabled: true,
+        });
+
+        const group_stop = new Adw.PreferencesGroup();
+        group_stop.set_title(_('On extension stop'));
+        page.add(group_stop);
+
+        // addBooleanRow(settings, _('Don\'t enable unredirect on extension stop'), 'dont-enable-unredirect-on-extension-stop', group_stop, _("Enabling unredirection on extension stop may leak window info after locking screen in multi-screen system."));
+
+        const group_misc = new Adw.PreferencesGroup();
+        group_misc.set_title(_('Miscellaneous'));
+        page.add(group_misc);
+
+        // addBooleanRow(settings, _('Monkey patch unredirection functions'),
+        //     'monkey-patch-unredirect', group_misc, _("Monkey-patch Meta.enable_unredirect_for_display() and Meta.disable_unredirect_for_display() to no-op. This will only take effect on the next extension startup."));
+
+        // Add our page to the window
+        window.add(page);
     }
 }
-
-function fillPreferencesWindow(window) {
-    // Use the same GSettings schema as in `extension.js`
-    const settings = ExtensionUtils.getSettings(
-        'org.gnome.shell.extensions.taoky-customization');
-    
-    // Create a preferences page and group
-    const page = new Adw.PreferencesPage();
-    const group_startup = new Adw.PreferencesGroup();
-    group_startup.set_title(_('On extension startup'));
-    page.add(group_startup);
-
-    addBooleanRow(settings, _('Disable unredirection'), 'disable-unredirect', group_startup);
-    addBooleanRow(settings, _('Disable xwayland pointer gestures'), 'disable-xwayland-pointer-gestures', group_startup);
-    addBooleanRow(settings, _('Disable minimize animation'), 'disable-minimize-animation', group_startup);
-
-    const group_stop = new Adw.PreferencesGroup();
-    group_stop.set_title(_('On extension stop'));
-    page.add(group_stop);
-
-    addBooleanRow(settings, _('Don\'t enable unredirect on extension stop'), 'dont-enable-unredirect-on-extension-stop', group_stop, _("Enabling unredirection on extension stop may leak window info after locking screen in multi-screen system."));
-
-    const group_misc = new Adw.PreferencesGroup();
-    group_misc.set_title(_('Miscellaneous'));
-    page.add(group_misc);
-
-    addBooleanRow(settings, _('Monkey patch unredirection functions'),
-        'monkey-patch-unredirect', group_misc, _("Monkey-patch Meta.enable_unredirect_for_display() and Meta.disable_unredirect_for_display() to no-op. This will only take effect on the next extension startup."));
-
-    // Add our page to the window
-    window.add(page);
-}
-
